0c82bbaed71ff99d32aba94946976800e6280b4d
Jake <jake.arefyev@gmail.com>
2023-04-12 23:24:51 +0300

    Update app.module.ts

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormlyModule } from '@ngx-formly/core'
+import { FormlyMaterialModule } from '@ngx-formly/material'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './components/app/app.component'
@@ -12,7 +14,17 @@ import { LayoutModule } from './core/layout/layout.module'
 
 @NgModule({
   declarations: [AppComponent, ErrorComponent, OutletComponent, HomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    FormlyMaterialModule,
+    FormlyModule.forRoot(),
+
+    AppRoutingModule,
+    LayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
